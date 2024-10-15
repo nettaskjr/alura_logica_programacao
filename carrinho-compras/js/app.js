7bb3fdcd28eb3c24ad1a88e6b07c99947233da83
NESTOR GONCALVES DE RESENDE JUNIOR <nestor.junior@gmail.com>
2024-10-15 16:11:34 -0300

    ...

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -2,11 +2,12 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
     
-    alert(nomeProduto);
-    alert(quantidade.value);
-
     let preco = quantidade.value * valorUnitario;
-    alert(preco);
+    let carrinho = document.getElementById('lista-produtos');
+
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
 }
